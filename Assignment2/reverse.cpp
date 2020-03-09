4667a51ce86182f6d9fc54a843510a23ff0415ae
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-03-09 15:13:22 -0700

    Update reverse.cpp

--- Assignment2/reverse.cpp
+++ Assignment2/reverse.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <iomanip>
 
+extern "C" void reverse(long [], long);
+using namespace std;
+
+void reverse(long arr[], long n)
+{
+  for (int i = 0; i < n; i++);
+  {
+    cout << setw(20) << arr[i] << endl;
+  }
+}
